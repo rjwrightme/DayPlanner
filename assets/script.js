680d4c4e5b5e4a57ffe24c9ae560ca1ef73f591a
RJ Wright <rj@rjwright.me>
2020-09-12 20:53:20 +1000

    Add popup functionality to planner when user clicks a time slot

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
-const todayDate = document.querySelector('#todayDate');
-const todayDay = document.querySelector('#todayDay');
 const m = moment();
 
 // Set Today's Date using Moment JS
-todayDate.innerHTML = `<span>${m.format("Do MMMM")}</span> ${m.format("YYYY")}`;
-todayDay.textContent = m.format("dddd");
+$('#todayDate').html(`<span>${m.format("Do MMMM")}</span> ${m.format("YYYY")}`);
+$('#todayDay').text(m.format("dddd"));
+
+
+function editEvent(hour) {
+    //show popup
+    console.log(hour);
+    $('#popUp').show();
+    $('body').css('overflow', 'hidden');
+
+    // hide popup if user clicks away
+    $('#popUp').click(function (e) {
+        if (e.target.id === 'popUp') {
+            $('#popUp').hide();
+            $('body').css('overflow', 'scroll');
+        }
+    });
+}
+
+function currentHour() {
+    // add correct styles to hour blocks
+}
+
+// Event Handlers
+$('.hourBlock').click(function (e) {
+    editEvent(e.target.id);
+});
